a4621ba305ae04a8de18e7e7c6a04ccb473fe477
lazare bordereaux <lazare.bordereaux@edu.devinci.fr>
2024-03-26 14:18:32 +0100

    final tests

--- src/nodes/node.ts
+++ src/nodes/node.ts
@@ -1,37 +1,25 @@
 import bodyParser from "body-parser";
 import express from "express";
 import { BASE_NODE_PORT } from "../config";
-import { Value } from "../types";
+import { NodeState, Value } from "../types";
+import axios from 'axios';
+import * as console from "console";
+import {delay} from "../utils";
 
-type NodeState = {
-  killed: boolean;
-  x: 0 | 1 | "?" | null;
-  decided: boolean | null;
-  k: number | null;
-};
 
 export async function node(
-  nodeId: number, // the ID of the node
-  N: number, // total number of nodes in the network
-  F: number, // number of faulty nodes in the network
-  initialValue: Value, // initial value of the node
-  isFaulty: boolean, // true if the node is faulty, false otherwise
-  nodesAreReady: () => boolean, // used to know if all nodes are ready to receive requests
-  setNodeIsReady: (index: number) => void // this should be called when the node is started and ready to receive requests
+    nodeId: number, // the ID of the node
+    N: number, // total number of nodes in the network
+    F: number, // number of faulty nodes in the network
+    initialValue: Value, // initial value of the node
+    isFaulty: boolean, // true if the node is faulty, false otherwise
+    nodesAreReady: () => boolean, // used to know if all nodes are ready to receive requests
+    setNodeIsReady: (index: number) => void // this should be called when the node is started and ready to receive requests
 ) {
   const node = express();
   node.use(express.json());
   node.use(bodyParser.json());
 
-  let currentState: NodeState = {
-    killed: false,
-    x: initialValue,
-    decided: false,
-    k: null
-  };
-
-  // TODO implement this
-  // this route allows retrieving the current status of the node
   node.get("/status", (req, res) => {
     if (isFaulty) {
       res.status(500).send("faulty");
@@ -40,42 +28,113 @@ export async function node(
     }
   });
 
-  // TODO implement this
-  // this route allows the node to receive messages from other nodes
-  // node.post("/message", (req, res) => {});
 
-  // TODO implement this
-  // this route is used to start the consensus algorithm
-  // node.get("/start", async (req, res) => {});
+  let nodeState: NodeState = {
+    killed: false,
+    x: null,
+    decided: null,
+    k: null
+  };
 
-  // TODO implement this
-  // this route is used to stop the consensus algorithm
-  // node.get("/stop", async (req, res) => {});
+  let messagesR: Map<number, any[]> = new Map();
+  let messagesP: Map<number, any[]> = new Map();
 
-  // TODO implement this
-  // get the current state of a node
-  // Route to get the current state of the node
-  // Route to get the current state of the node
-  node.get("/getState", (req, res) => {
-    if (isFaulty) {
-      currentState = {
-        killed: true,
-        x: null,
-        decided: null,
-        k: null
-      };
+  node.post("/message", async (req, res) => {
+    let { k, x, messageType } = req.body;
+    if (!isFaulty && !nodeState.killed) {
+      if (messageType == "R") {
+        if (!messagesR.has(k)) {
+          messagesR.set(k, []);
+        }
+        messagesR.get(k)!.push(x);
+        let messageR = messagesR.get(k)!;
+        if (messageR.length >= (N - F)) {
+          let count0 = messageR.filter((el) => el == 0).length;
+          let count1 = messageR.filter((el) => el == 1).length;
+          let newX = "?"; //undecided is  default
+          if (count0 > (N / 2)) {
+            newX = "0";
+          } else if (count1 > (N / 2)) {
+            newX = "1";
+          }
+          for (let i = 0; i < N; i++) {
+            axios.post(`http://localhost:${BASE_NODE_PORT + i}/message`, {k: k, x: newX, messageType: "P"});
+          }
+        }
+      } else if (messageType == "P") {
+        if (!messagesP.has(k)) {
+          messagesP.set(k, []);
+        }
+        messagesP.get(k)!.push(x);
+        let messageP = messagesP.get(k)!;
+        if (messageP.length >= N - F) {
+          let count0 = messageP.filter((el) => el == 0).length;
+          let count1 = messageP.filter((el) => el == 1).length;
+
+          if (count0 >= F + 1) {
+            nodeState.x = 0;
+            nodeState.decided = true;
+          } else if (count1 >= F + 1) {
+            nodeState.x = 1;
+            nodeState.decided = true;
+          } else {
+            if (count0 + count1 > 0 && count0 > count1) {
+              nodeState.x = 0;
+            } else if (count0 + count1 > 0 && count0 < count1) {
+              nodeState.x = 1;
+            } else {
+              nodeState.x = Math.random() > 0.5 ? 0 : 1;
+            }
+            nodeState.k = k + 1;
+            for (let i = 0; i < N; i++) {
+              axios.post(`http://localhost:${BASE_NODE_PORT + i}/message`, {k: k + 1, x: nodeState.x, messageType: "R"});
+            }
+          }
+        }
+      }
     }
-    res.status(200).send({ x: currentState.x, k: currentState.k, isFaulty, decided: currentState.decided });
+    res.status(200).send("message received");
   });
 
 
-  // start the server
-  const server = node.listen(BASE_NODE_PORT + nodeId, async () => {
-    console.log(
-      `Node ${nodeId} is listening on port ${BASE_NODE_PORT + nodeId}`
-    );
+  node.get("/start", async (req, res) => {
 
-    // the node is ready
+    while (!nodesAreReady()) {
+      await delay(5);
+    }
+
+    if (!isFaulty) {
+      nodeState.decided = false;
+      nodeState.x = initialValue;
+      nodeState.k = 1;
+      //sends a proposal message (R, k, x) to all other processes
+      for (let i = 0; i < N; i++) {
+        await axios.post(`http://localhost:${BASE_NODE_PORT + i}/message`, {
+          k: nodeState.k,
+          x: nodeState.x,
+          messageType: "R"
+        });
+      }
+    }
+    else {
+      nodeState.decided = null;
+      nodeState.x = null;
+      nodeState.k = null;
+    }
+    res.status(200).send("started");
+  });
+
+  node.get("/stop", (req, res) => {
+    nodeState.killed = true;
+    res.status(200).send("killed");
+  });
+
+  node.get("/getState", (req, res) => {
+    res.status(200).send({ x: nodeState.x, k: nodeState.k, killed: nodeState.killed, decided: nodeState.decided });
+  });
+
+  const server = node.listen(BASE_NODE_PORT + nodeId, async () => {
+    console.log(`Node ${nodeId} is listening on port ${BASE_NODE_PORT + nodeId}`);
     setNodeIsReady(nodeId);
   });
 
